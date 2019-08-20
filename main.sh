d2d99558e56007d6b91da32524722afac2147fc5
customer <customer10123@localhost.localdomain>
2019-08-20 16:43:59 +0200

    Added second way to check Fedora version, more comments, adding and checking user jenkins, file with Hello world

--- main.sh
+++ main.sh
@@ -19,21 +19,37 @@ rlJournalStart
             rlRun 'TmpDir=$(mktemp -d)' 0 'Creating tmp directory'
             rlRun "pushd $TmpDir"
         rlPhaseEnd
-
+# Test phase: Running required tasks
         rlPhaseStartTest
+	    #First way to check if you're running Fedora 30 
 	    rlIsFedora 30 
 	    if [ $? -eq 0 ]; then
 		    rlPass "You are running Fedora 30"
 	    else
 		    rlFail "You are not running Fedora 30"
 	    fi
+	    #Second way to check if you're running Fedora 30
+	    FED30=`cat /etc/fedora-release | grep 30 | wc -l`
+	    if [ $FED30 -eq 1 ]; then
+		    rlPass "You are running Fedora 30"
+            else
+		    rlFail "You are not running Fedora 30"
+	    fi
+	    #Check for lorax-composer. Install if not found.
 	    if [ rlCheckRpm lorax-composer ]; then
 		    rlAssertRpm lorax-composer
 	    else
 		    yum install lorax-composer
 		    rlAssertRpm lorax-composer
 	    fi
+	    #Check if /var/lib/lorax/composer exists
 	    rlAssertExists "/var/lib/lorax/composer"
+	    #Adding user jenkins
+	    useradd jenkins
+	    #Checking if user jenkins exists
+	    rlRun 'compgen -u | grep jenkins' 0 "User Jenkins exists"
+	    #Creating a file Hello.txt with "Hello World".
+	    rlRun 'echo "Hello World" >> Hello.txt' 
         rlPhaseEnd
 
 	# Cleanup phase: Remove test directory
