ecac3e56cd1449c19524de2a12dbd51011e3f480
customer <customer10123@localhost.localdomain>
2019-08-20 14:03:02 +0200

    Added one way to check if Fedora version is 30

--- main.sh
+++ main.sh
@@ -10,8 +10,10 @@ else
 	sudo dnf -y install beakerlib-libraries
 fi
 
+#including beakerlib
 . /usr/share/beakerlib/beakerlib.sh
 
+#small example using beakerlib
 rlJournalStart
         rlPhaseStartTest
             rlAssertRpm "setup"
@@ -19,3 +21,15 @@ rlJournalStart
             rlAssertGrep "root" "/etc/passwd"
         rlPhaseEnd
 rlJournalEnd
+
+#check Fedora version from fedora-release file
+FED30=`cat /etc/fedora-release | grep 30 | wc -l` 
+if [ -f /etc/fedora-release ]; then
+	if [ $FED30 -eq 1 ]; then 
+		echo "You are running Fedora 30."
+	else
+		echo "You are not running Fedora 30."
+	fi
+else
+	echo "You are not using Fedora"
+fi	
